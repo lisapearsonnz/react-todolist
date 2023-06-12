2fe808cdc7b80458a3edb7ec771cf9ea36ac2500
lisapearsonnz <lisapearson.nz@gmail.com>
2023-06-12 20:24:39 +1200

    style fix

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from'./Todo'
 
 const style = {
-  bg:'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]'
+  bg:'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]',
+  container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
+  heading: 'text-3xl font-bold text-center text-gray-800 p-2',
+  form: 'flex justify-between',
+  input: 'border p-2 w-full text-xl',
+  button: 'border p-4 ml-2 bg-purple-500 text-slate-100',
+  count:'text-center p-2'
 }
 
 function App() {
-  const [todos, setTodos] = useState(['Learn React, Grind Leetcode'])
+  const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode'])
   return (
     <div className={style.bg}>
       <div className={style.container}>
-        <h3 className={style.heading}></h3>
+        <h3 className={style.heading}>To-do List</h3>
         <form className={style.form}>
             <input type ='text' placeholder = 'Add To-Do' />
           <button className={style.button}><AiOutlinePlus size={30} /></button>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import {FaRegTrashAlt} from 'react-icons/fa'
 
 const style ={
-
+li: 'flex justify-between bg-slate-200 p-4 my-2 capitalize',
+liComplete:'flex justify-between bg-slate-400 p-4 my-2 capitalize',
+row: 'flex',
+text:'ml-2 cursor-pointer',
+textComplete: 'ml-2 cursor-pointer line-through',
+button:'cursor-pointer flex items-center'
 
 }
 
@@ -13,7 +18,6 @@ const Todo = ({todo}) => {
         <input type='checkbox' />
             <p className={style.text}>{todo}</p>
     </div>
-
         <button>{<FaRegTrashAlt />}</button>
    </li>
   )
