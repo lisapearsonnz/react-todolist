12b6a76b3c7a1e473f63d8eda2d88d7d6a7db9bc
lisapearsonnz <lisapearson.nz@gmail.com>
2023-06-12 20:12:48 +1200

    App structure

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from'./Todo'
+
+const style = {
+  bg:'h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]'
+}
+
+function App() {
+  const [todos, setTodos] = useState(['Learn React, Grind Leetcode'])
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}></h3>
+        <form className={style.form}>
+            <input type ='text' placeholder = 'Add To-Do' />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <ul>
+          {todos.map((todo, index)=> (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+          <p className={style.count}>You have 2 todos</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
